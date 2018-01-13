a85cfad7b4932a27709af25d1baf402fdad2a640
Chirdon <nodrihc93@gmail.com>
2018-01-13 21:40:57 +0000

    Entity Etudiant just created

---
+++ src/OC/UserBundle/Entity/Etudiant.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace OC\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Etudiant
+ *
+ * @ORM\Table(name="etudiant")
+ * @ORM\Entity(repositoryClass="OC\UserBundle\Repository\EtudiantRepository")
+ */
+class Etudiant extends User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="universite", type="string", length=255)
+     */
+    private $universite;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="addresse", type="string", length=255)
+     */
+    private $addresse;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set universite
+     *
+     * @param string $universite
+     *
+     * @return Etudiant
+     */
+    public function setUniversite($universite)
+    {
+        $this->universite = $universite;
+
+        return $this;
+    }
+
+    /**
+     * Get universite
+     *
+     * @return string
+     */
+    public function getUniversite()
+    {
+        return $this->universite;
+    }
+
+    /**
+     * Set addresse
+     *
+     * @param string $addresse
+     *
+     * @return Etudiant
+     */
+    public function setAddresse($addresse)
+    {
+        $this->addresse = $addresse;
+
+        return $this;
+    }
+
+    /**
+     * Get addresse
+     *
+     * @return string
+     */
+    public function getAddresse()
+    {
+        return $this->addresse;
+    }
+}
+
