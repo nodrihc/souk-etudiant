d18a044105ded1e6f222149bd4a4e18d943de728
Chirdon <nodrihc93@gmail.com>
2018-01-13 22:14:27 +0000

    proprio added

---
+++ src/OC/UserBundle/Entity/Proprietaire.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace OC\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Proprietaire
+ *
+ * @ORM\Table(name="proprietaire")
+ * @ORM\Entity(repositoryClass="OC\UserBundle\Repository\ProprietaireRepository")
+ */
+class Proprietaire
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="numero", type="integer")
+     */
+    private $numero;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param integer $numero
+     *
+     * @return Proprietaire
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return int
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+}
+
